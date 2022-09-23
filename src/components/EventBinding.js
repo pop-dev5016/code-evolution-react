f5541af6d9b4d3ba0e15136081b9923f6d09edca
devpop <impopcity@gmail.com>
2022-09-23 21:57:46 +0530

    4th method

--- src/components/EventBinding.js
+++ src/components/EventBinding.js
@@ -7,14 +7,18 @@ import React, { Component } from 'react'
       this.state = {
          message:"hello"
       }
-      this.clickhandler = this.clickhandler.bind(this)
+    //   this.clickhandler = this.clickhandler.bind(this)
     }
-    clickhandler(){
+//     clickhandler(){
+//         this.setState({
+//             message:"GOOD BYE"
+//     })
+//  }
+     clickhandler = () =>{
         this.setState({
-            message:"GOOD BYE"
-    })
-}
-    
+            message:"Goodbye!!!!!!!!!!!!!"
+        })
+     }
   render() {
     return (
       <div>
