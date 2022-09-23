376a8e0ee1f10b750d106603fa6c5b68ecb01b17
devpop <impopcity@gmail.com>
2022-09-23 18:42:20 +0530

    setState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,12 +11,27 @@ class Counter extends Component {
     }
 
     increment(){
-        this.setState({
-            count: this.state.count+1
-        },()=>{
-            console.log("callback + value : ",this.state.count);
-        })
-       console.log("state veliya + ",this.state.count);
+    //     this.setState({
+    //         count: this.state.count+1
+    //     },()=>{
+    //         console.log("callback + value : ",this.state.count);
+    //     })
+    //    console.log("state veliya + ",this.state.count);
+
+     this.setState(prevState=>({    ////(one pervious state,props)
+        count:prevState.count +1        ///(prevstae.count+props.addvalue)
+     }))
+
+     console.log(this.state.count);
+    }
+
+    incrementFive(){
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+    
     }
 
 
@@ -34,7 +49,7 @@ class Counter extends Component {
      <div>
         <button onClick={()=>this.decrement()}>decrement</button>
         <div>count-{this.state.count}</div>
-        <button onClick={()=>this.increment()}>increment</button>
+        <button onClick={()=>this.incrementFive()}>increment</button>
      </div>
     
     )
